ff24969da816394314d107be4c25663cf28c5d3a
Jaypro64 <154343234+Jaypro64@users.noreply.github.com>
2026-09-05 02:26:25 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'origins-dating-simualtor-demo';
+var cacheName = 'origins-dating-simualtor';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
